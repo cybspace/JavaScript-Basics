15f84b236fd1d9005861f6436810939fcbae0f38
Колонцов Д.Ю <dkolontsov@custis.ru>
2016-09-06 18:23:17 +0300

    created common view - index.html created riddlesProgram - updating view and run some code.

---
+++ javascript/start.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dkolontsov on 06.09.2016.
+ */
+window.onload = function () {
+
+    //index.html
+    document.getElementById('intro').innerHTML = "Приветствую! Меня зовут <strong>KODI</strong> и я - <em>начинающий программист</em>. Этот сайт я создал на бесплатных уроках от <a id=text_link href=https://geekbrains.ru/>Geek Brains</a>.";
+    document.getElementById('result').innerHTML = "На этом сайте вы можете посмотреть програмки, которые я написал:";
+
+    //"program" calls on link clicks
+    document.getElementById('riddlesProgram_1').onclick = riddlesProgram;
+    document.getElementById('riddlesProgram_2').onclick = riddlesProgram;
+
+}
