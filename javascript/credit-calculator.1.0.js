31ea0436d1803a39bf4b96167c68c7e91ed700d5
Колонцов Д.Ю <dkolontsov@custis.ru>
2016-09-14 17:22:16 +0300

    fully rewrited credit Calc

--- javascript/credit-calculator.1.0.js
+++ javascript/credit-calculator.1.0.js
@@ -4,75 +4,38 @@
 function creditCalculatorProgram () {
     document.getElementById('pageName').innerHTML = "Кредитный калькулятор";
     document.getElementById('intro').innerHTML = "Я помогу расчитать график платежей по кредиту.";
-    document.getElementById('input_1').innerHTML = "";
-    document.getElementById('input_2').innerHTML = "";
-    document.getElementById('input_3').innerHTML = "";
+    document.getElementById('output_1').innerHTML = "";
+    document.getElementById('output_2').innerHTML = "";
+    document.getElementById('output_3').innerHTML = "";
     document.getElementById('result').innerHTML = "";
     document.getElementById('programLinks').innerHTML = "<a href=# class=inboxLink id=loanInput>Задать сумму кредита</a>/<a href=# class=inboxLink id=interestInput>Задать % ставку</a>/<a href=# class=inboxLink id=timeInput>Задать срок</a>/<a href=# class=inboxLink id=calculate>Рассчитать!</a>";
 
     var loan, interest, time;
-    var countLoan = 0, countInterest = 0, countTime = 0;
-    // Вводим сумму кредита
-    var loanInput = document.getElementById('loanInput');
-    loanInput.onclick = function() {
-        loan = +prompt("Введите сумму кредита:");
-        while (loan <= 0 || isNaN(loan)) {
-            loan = +prompt("Неверное значение. \n\n1. Сумма должна быть больше нуля. \n2. Для дробной части используйте точку. \n\nПовторите ввод:");
-        }
-        if (countLoan == 0) {
-            countLoan++;
-            document.getElementById('input_1').innerHTML = "<span id=loan>Сумма кредита: <span id=loanAmount>" + loan.toFixed(2) + "</span>.</span>";
-            document.getElementById('result').innerHTML = "";
+
+    var inputCheck = function (isFull) {
+        if (isFull === undefined) isFull = false;
+        var number = +prompt("Введите значение:");
+        if (isFull) {
+            while (isNaN(number) || number <= 0 || (number - number.toFixed(0)) !== 0) {
+                number = +prompt("Неверное значение! \n\n1. Значение должно быть больше нуля. \n2. Это должно быть целое число. \n\nПовторите ввод:");
+            }
         }
         else {
-            document.getElementById('loanAmount').innerHTML = loan;
-            document.getElementById('result').innerHTML = "";
+            while (isNaN(number) || number <= 0) {
+                number = +prompt("Неверное значение! \n\n1. Значение должно быть больше нуля. \n2. Для десятичной части используйте точку. \n\nПовторите ввод:");
+            }
         }
+        return number;
     };
-    // Вводим сумму процентов
-    var interestInput = document.getElementById('interestInput');
-    interestInput.onclick = function() {
-        interest = +prompt("Введите процентную ставку по кредиту:");
-        while (interest < 0 || isNaN(interest)) {
-            interest = +prompt("Неверное значение. \n\n1. Процентная ставка не может быть меньше нуля. \n2. Для дробной части используйте точку. \n3. Не указывайте знак процентов. \n\nПовторите ввод:");
-        }
-        if (countInterest == 0) {
-            countInterest++;
-            document.getElementById('input_2').innerHTML = "<span id=interest>Процентная ставка: <span id=interestAmount>" + interest.toFixed(2) + "</span>%.</span>";
-            document.getElementById('result').innerHTML = "";
-        }
-        else {
-            document.getElementById('interestAmount').innerHTML = interest;
-            document.getElementById('result').innerHTML = "";
-        }
-    };
-    // Вводим срок
-    var timeInput = document.getElementById('timeInput');
-    timeInput.onclick = function() {
-        time = +prompt("Введите срок в месяцах:");
-        while (time <= 0 || isNaN(time) || (time - time.toFixed(0) != 0)) {
-            time = +prompt("Неверное значение. \n\n1. Срок должен быть больше нуля. \n2. Вводите целое число (без дробной части). \n\nПовторите ввод:");
-        }
-        if (countTime == 0) {
-            countTime++;
-            document.getElementById('input_3').innerHTML = "<span id=time>Срок кредитования в месяцах: <span id=timeAmount>" + time + "</span>.</span>";
-            document.getElementById('result').innerHTML = "";
-        }
-        else {
-            document.getElementById('timeAmount').innerHTML = time;
-            document.getElementById('result').innerHTML = "";
-        }
-    };
-    // Рассчитываем график
-    var calculate = document.getElementById('calculate');
-    calculate.onclick = function() {
+    
+    var creditCalc = function (loan, interest, time) {
         if (isNaN(loan) || isNaN(interest) || isNaN(time)) {
             document.getElementById('result').innerHTML = "<span style=color:red;>Не заданы праметры для расчета или заданы не все параметры: сумма кредита, процентная ставка или срок. </span><br/>"
         }
         else {
             var k = interest / 100;
-            var monthInterest = k/12;
-            var monthPayment = loan * (monthInterest + monthInterest/(Math.pow(1 + monthInterest, time) - 1));
+            var monthInterest = k / 12;
+            var monthPayment = loan * (monthInterest + monthInterest / (Math.pow(1 + monthInterest, time) - 1));
             var paymentLoan, paymentInterest, loanRest;
             loanRest = loan;
 
@@ -91,5 +54,38 @@ function creditCalculatorProgram () {
             }
             document.getElementById('result').innerHTML += "<br/>Итого переплата по кредиту: " + ( monthPayment * time - loan ).toFixed(2) + ".";
         }
+    };
+
+    var outputBuilder = function(message, name, number) {
+        if (message === undefined || name === undefined || number === undefined) {
+            return "Error";
+        }
+        else {
+            document.getElementById('output_' + number).innerHTML = "<span id=" + name + ">" + message + ": <span id=" + name + "Amount></span>.</span>";
+            document.getElementById('result').innerHTML = "";
+        }
+    };
+
+    document.getElementById('loanInput').onclick = function () {
+        outputBuilder("Сумма кредита, в руб.", "loan", 1);
+        loan = inputCheck();
+        document.getElementById('loanAmount').innerHTML = loan;
+    };
+
+    document.getElementById('interestInput').onclick = function () {
+        outputBuilder("Процентная ставка, в %", "interest", 2);
+        interest = inputCheck();
+        document.getElementById('interestAmount').innerHTML = interest;
+    };
+
+    document.getElementById('timeInput').onclick = function () {
+        outputBuilder("Срок кредита, в годах", "time", 3);
+        time = inputCheck();
+        document.getElementById('timeAmount').innerHTML = time;
+    };
+
+    document.getElementById('calculate').onclick = function () {
+        creditCalc(loan, interest, time);
     }
+    
 }
